505ae1a1288b2279dbf83a73bcec59a5aadd4b98
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2026-04-20 13:08:14 +0300

    chore: Bump deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,18 +2,7 @@ import jest from "eslint-plugin-jest";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import github from "eslint-plugin-github";
 
 export default [{
       ignores: [
@@ -26,7 +15,7 @@ export default [{
         "**/*.cts",
         "**.*.mts"
       ],
-    }, ...compat.extends("plugin:github/recommended"), {
+    }, github.getFlatConfigs().recommended, {
     plugins: {
         jest,
         "@typescript-eslint": typescriptEslint,
